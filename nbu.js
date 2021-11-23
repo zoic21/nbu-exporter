3bb1e2ced09d87ff1b69bbf42b458b21ba3c663e
Loïc <loic@jeedom.com>
2021-11-23 11:59:30 +0100

    test

--- nbu.js
+++ nbu.js
@@ -1,27 +1,45 @@
 const config = require('config').config;
 const axios = require('axios').default;
 
-var NBU = {}
-
-NBU.login = function(_callback){
-    axios.post(config.nbu.url, 
-      {
-        "domainType":"vx",
-        "domainName":config.nbu.domain,
-        "userName":config.nbu.username,
-        "password":config.nbu.password
-      },
-      { 
-          headers:  {'content-type': 'application/vnd.netbackup+json;version=1.0'}
-      })
-      .then(function (response) {
-        console.log(response);
-        _callback(reponse)
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+var NBU = {
+    token : null,
+    expire : null,
+    admin : {}
 }
 
+NBU.axios = axios.create({
+    httpsAgent: new https.Agent({  
+        rejectUnauthorized: false
+    })
+});
+
+NBU.login = function(_callback){
+    NBU.axios.post(config.nbu.url+'/login',{"userName":config.nbu.username,"password":config.nbu.password},{ 
+        headers:  {'content-type': 'application/vnd.netbackup+json;version=1.0'}
+    }).then(function (response) {
+        NBU.token = response.data.token
+        NBU.expire = response.data.validity + Math.floor(new Date().getTime() / 1000)
+        _callback()
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+NBU.admin.job = function(_callback){
+    NBU.axios.get(config.nbu.url+'/admin/job',{ 
+        headers:  {'content-type': 'application/vnd.netbackup+json;version=1.0','Authorization' : NBU.token}
+    }).then(function (response) {
+        _callback(response)
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+
+NBU.login(function(){
+    NBU.admin.job(function(response){
+        console.log(response);
+    })
+})
 
 exports.nbu = NBU
