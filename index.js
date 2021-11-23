c68ed1cf3dab202ec3f6771dd07737b28426182c
Loïc <loic@jeedom.com>
2021-11-23 10:59:45 +0100

    add code

---
+++ index.js
@@ -0,0 +1,5 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+const session = require('express-session');
+const fs = require('fs');
